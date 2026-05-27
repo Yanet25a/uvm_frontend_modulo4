1191536490ebcdbd2262d1bc7db1d30852f7439f
Yanet25a <yanet25@gmail.com>
2026-05-27 10:35:19 -0600

    2version

--- Proyecto/consultaClimaApi/src/routes/weather.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getWeather } from "../controllers/weather.controller.js";
-
-const router = Router();
-
-router.get("/", getWeather);
-
-export default router;
